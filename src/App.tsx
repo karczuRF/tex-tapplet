5ef3969340e8fb38628524fbfa04b9c4ed67f0a0
karczuRF <oskar.karcz@rumblefish.pl>
2025-01-24 13:37:11 +0100

    feat: add dex components

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css"
+import { Box, Paper, Typography } from "@mui/material"
+import { ExitPool, JoinPool, Swap } from "./Components"
 function App() {
-  const [count, setCount] = useState(0)
-
+  const handlePlaceholder = async (): Promise<void> => {}
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Box display="flex" justifyContent="center" alignItems="center" gap={6} flexDirection="column">
+      <Box display="flex" justifyContent="center" alignItems="center" height="100%">
+        <Paper variant="outlined" elevation={0} sx={{ padding: 4, margin: 3, borderRadius: 4 }}>
+          <Typography variant="h6" textAlign="center">
+            Pool state
+          </Typography>
+          <Typography>Pool token A balance: </Typography>
+          <Typography>Pool token B balance: </Typography>
+          <Typography>LP token total supply: </Typography>
+        </Paper>
+        <Paper variant="outlined" elevation={0} sx={{ padding: 4, margin: 3, borderRadius: 4 }}>
+          <Typography variant="h6" textAlign="center">
+            Your balances
+          </Typography>
+          <Typography>Your token A balance: </Typography>
+          <Typography>Your token B balance: </Typography>
+          <Typography>Your LP token balance: </Typography>
+        </Paper>
+      </Box>
+      <Box display="flex" justifyContent="center" alignItems="center" width="100%" height="100%" gap={4} flexGrow={1}>
+        <JoinPool onSubmit={handlePlaceholder} callback={handlePlaceholder} />
+        <ExitPool onSubmit={handlePlaceholder} callback={handlePlaceholder} />
+        <Swap handleSwap={handlePlaceholder} callback={handlePlaceholder} />
+      </Box>
+    </Box>
   )
 }
 
